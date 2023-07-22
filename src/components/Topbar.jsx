6d4814b574523f7234b2f20531e068dd8f87c641
Monil Sharma <68782355+monil-sharma@users.noreply.github.com>
2023-07-22 23:41:50 +0530

    topbar completed

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -5,39 +5,59 @@ export default function Topbar() {
   return (
     <div className="p-3 h-12 bg-smBlue flex  items-center w-full mx-auto">
       <div className="left flex-3">
-        <div className="logo text-white text-xl ">The Social Network</div>
+        <div className="logo text-white text-xl cursor-pointer hover:text-slate-200">
+          The Social Network
+        </div>
       </div>
       <div className="center  flex-5">
-        <div className="search h-7 flex items-center bg-white w-full rounded-2xl">
-          <Search className="m-1 !text-xl" />
+        <div className="searchBar h-[1.875rem] flex items-center bg-white w-full rounded-2xl">
+          <Search className="m-2 !text-xl" />
           <input
             type="text"
             placeholder="Search for friends, post or videos"
-            className="searchBar w-4/5"
+            className="searchInput w-4/5 focus:outline-none"
           />
         </div>
       </div>
-      <div className="right flex-4 flex justify-end">
-        <div className="links flex items-center">
-          <div className="link">Homepage</div>
-          <div className="link">Timeline</div>
+      <div className="right flex-4 flex justify-around text-white items-center">
+        <div className="links cursor-pointer flex items-center m-2">
+          <div className="link text-sm m-1">Homepage</div>
+          <div className="link text-sm m-1">Timeline</div>
         </div>
-        <div className="icons flex items-center">
-          <div className="icon">
-            <Person />1
+        <div className="icons flex items-center hover:cursor-pointer">
+          <div className="icon relative mr-4">
+            <Person className="" />
+            <span
+              className="iconBadge h-4 w-4 bg-red-600 rounded-full absolute text-xs text-center"
+              style={{ top: -5, right: -5 }}
+            >
+              1
+            </span>
           </div>
-          <div className="icon">
-            <Chat /> 2
+          <div className="icon relative mr-4">
+            <Chat />{" "}
+            <span
+              className="iconBadge h-4 w-4 bg-red-600 rounded-full absolute text-xs text-center"
+              style={{ top: -5, right: -5 }}
+            >
+              2
+            </span>
           </div>
-          <div className="icon">
-            <Notifications /> 3
+          <div className="icon relative mr-4 ">
+            <Notifications />{" "}
+            <span
+              className="iconBadge h-4 w-4 bg-red-600 rounded-full absolute text-xs text-center"
+              style={{ top: -5, right: -5 }}
+            >
+              3
+            </span>
           </div>
         </div>
         <div className="signout">
           <img
             src={signoutImg}
             alt="user Image"
-            className="rounded-full w-8 h-8"
+            className="rounded-full w-8 h-8 object-cover cursor-pointer"
           />
         </div>
       </div>
